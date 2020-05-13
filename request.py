29e6989723a220c7f3a069b792b25217a2cae5d3
Paulo <paulopsslmn@gmail.com>
2020-05-13 19:49:02 -0300

    Removed URL

--- request.py
+++ request.py
@@ -2,14 +2,14 @@
 import requests
 
 def getLight():
-    url = 'http://03670640:a5b8237720ca6068@broker.shiftr.io/light'
+    url = ''
     res = requests.get(url)
     if res.status_code != 200:
         print('Request code: ', res.status_code)
     return res.text
 
 def setLight(light_state):
-    url = 'http://03670640:a5b8237720ca6068@broker.shiftr.io/light'
+    url = ''
     if str(light_state) != getLight():
         res = requests.post(url, json=light_state)
         if res.status_code != 200:
